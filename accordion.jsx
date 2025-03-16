04713ed50a9d81b16519d8914148d503991bbc61
Aakash Pokhriyal <aakashpokhriyal7@gmail.com>
2025-03-16 21:51:53 +0530

    Add files via upload

---
+++ accordion.jsx
@@ -0,0 +1,23 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdCancel, MdAnalytics } from "react-icons/md";
+const data = [
+  {
+    icon: <HiShieldCheck/>,
+    heading: "Best interest rates on the market",
+    detail:
+      "Find the best real estate loan interest rates starting from 8.10% per annum and save big on your dream home! Compare top lenders and secure the lowest rates today!",
+  },
+  {
+    icon: <MdCancel />,
+    heading: "Prevent unstable prices",
+    detail:
+      "Ensure stable real estate prices with smart market insights and data-driven strategies. Stay ahead with secure investments and avoid price fluctuations!",
+  },
+  {
+    icon: <MdAnalytics />,
+    heading: "Best price on the market",
+    detail:
+      "Get the best real estate deals with unbeatable market prices on top properties! Buy or invest with confidence and maximize your savings today!",
+  },
+];
+export default data;
